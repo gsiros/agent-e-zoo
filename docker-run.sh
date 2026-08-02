6f5026dd9df02ed311a3c11b9391d4f9c05407ae
Georgios Syros <syros.g@northeastern.edu>
2026-08-01 21:16:42 -0400

    add default model

--- docker-run.sh
+++ docker-run.sh
@@ -6,6 +6,7 @@ xhost +local:docker 2>/dev/null || true
 docker run --rm -it \
     -e DISPLAY="$DISPLAY" \
     -e AUTOGEN_MODEL_API_KEY="$OPENAI_API_KEY" \
+    -e AUTOGEN_MODEL_NAME="gpt-4o" \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     --env-file .env \
     --network host \
